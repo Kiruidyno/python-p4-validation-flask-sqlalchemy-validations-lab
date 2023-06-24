de334dab9638886f2e22900f5e48f7f9f269a7eb
Kiruidyno <kirui472@gmail.com>
2023-06-24 13:18:18 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,24 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy import CheckConstraint
+
 db = SQLAlchemy()
+
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
-
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    name = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.String(10))  # Limit phone_number to 10 characters
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name field is required.")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be ten digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
@@ -27,6 +39,33 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must have at least 250 characters')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must be shorter or equal to 250 characters')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Category must be either "Fiction" or "Non-Fiction"')
+
+    __table_args__ = (
+        CheckConstraint(category.in_(['Fiction', 'Non-Fiction']), name='category_constraint'),
+    )
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f'Post(id={self.id}, title={self.title}, content={self.content}, summary={self.summary})'
